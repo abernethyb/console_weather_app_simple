1fd18a21b753a71d4138ab937bca27f28a81dba1
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2021-01-27 15:04:26 -0600

    added while loop to continue asking zip code until user decides to exit

--- Program.cs
+++ Program.cs
@@ -15,41 +15,52 @@ namespace consoleWeatherApp
             Console.WriteLine("Get a current weather update by U.S. postal code");
             Console.WriteLine("");
             Console.WriteLine("");
-            Console.Write("enter a valid U.S. zip code: ");
-            string sZip = Console.ReadLine();
-            //float lat = float.Parse(slat);
-            // float lon = float.Parse(slon);
-            // Console.WriteLine(lat);
-            // Console.WriteLine(lon);
-            //api.openweathermap.org/data/2.5/weather?zip=42240,us
-            var uri = $"https://api.openweathermap.org/data/2.5/weather?zip={sZip},us&units=imperial&appid={key.name}";
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders
-                .Accept
-                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var response = await httpClient.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
+            string sZip = "";
+            while (sZip != "Q" && sZip != "q")
             {
-                var json = await response.Content.ReadAsStreamAsync();
-                var weatherData = await JsonSerializer.DeserializeAsync<WeatherResponse>(json);
+                Console.Write("enter a valid U.S. zip code or enter 'q' to exit: ");
+                sZip = Console.ReadLine();
+                Console.Beep();
+                Console.Clear();
 
-                Console.WriteLine($"Current weather in: {weatherData.placeName}");
-                Console.WriteLine("");
-                Console.WriteLine("Current Conditions:");
-                Console.WriteLine("");
-                Console.WriteLine($"Current temperature: {weatherData.current.temp} degrees F");
-                foreach (Weather weather in weatherData.WeatherResponses)
+                if (sZip != "Q" && sZip != "q")
                 {
-                    Console.WriteLine($"{weather.main}");
-                    Console.WriteLine($"description: {weather.description}");
-                    Console.WriteLine("");
-                }
 
-            }
-            else
-            {
-                Console.WriteLine("Try that zip code one more time");
+                    var uri = $"https://api.openweathermap.org/data/2.5/weather?zip={sZip},us&units=imperial&appid={key.name}";
+                    var httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders
+                        .Accept
+                        .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var response = await httpClient.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStreamAsync();
+                        var weatherData = await JsonSerializer.DeserializeAsync<WeatherResponse>(json);
+
+                        Console.WriteLine("");
+                        Console.WriteLine($"Current weather in: {weatherData.placeName}");
+                        Console.WriteLine("");
+                        Console.WriteLine("Current Conditions:");
+                        Console.WriteLine("");
+                        Console.WriteLine($"Temperature: {weatherData.current.temp} degrees F");
+                        foreach (Weather weather in weatherData.WeatherResponses)
+                        {
+                            Console.WriteLine($"{weather.main}");
+                            Console.WriteLine($"Description: {weather.description}");
+                            Console.WriteLine("");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try that zip code one more time or enter 'q' to quit");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for checking the weather!");
+                }
             }
         }
     }
